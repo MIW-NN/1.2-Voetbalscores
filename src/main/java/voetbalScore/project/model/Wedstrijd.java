9b787e280e5419d2c1a6f3af0dabc8d563ca56a2
rutgerhanze <152165901+rutgerhanze@users.noreply.github.com>
2024-01-23 20:04:50 +0100

    Fill in the print classes

--- src/main/java/voetbalScore/project/model/Wedstrijd.java
+++ src/main/java/voetbalScore/project/model/Wedstrijd.java
@@ -21,7 +21,14 @@ public class Wedstrijd {
         }
     }
 
-    public void printUitslag() {}
+    public void printUitslag() {
+        System.out.println("Uitslag van de wedstrijd tegen " + naamTegenstander + ": " + voorDoelpunten + "-" + tegenDoelpunten);
+    }
 
-    public void printWedstrijdPunten() {}
+    public void printWedstrijdPunten() {
+        if (bepaalWedstrijdpunten() == 1) {
+            System.out.println("Dit levert 1 wedstrijdpunt op");
+        } else {
+            System.out.printf("Dit levert %d wedstrijdpunten op\n", bepaalWedstrijdpunten());
+        }}
 }
