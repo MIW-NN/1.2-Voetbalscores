74556c9395aefe5b096e230e54bfe64446de8941
rutgerhanze <152165901+rutgerhanze@users.noreply.github.com>
2024-01-23 19:33:50 +0100

    first part of the assignment

--- src/main/java/voetbalScore/project/model/Wedstrijd.java
+++ src/main/java/voetbalScore/project/model/Wedstrijd.java
@@ -1,9 +1,23 @@
 package voetbalScore.project.model;
 
 public class Wedstrijd {
-    public Wedstrijd(String naamTegenstander, int voorDoelpunten, int tegenDoelpunten) {}
+    private int tegenDoelpunten;
+    private int voorDoelpunten;
+    private String naamTegenstander;
+
+    public Wedstrijd(String naamTegenstander, int voorDoelpunten, int tegenDoelpunten) {
+        this.naamTegenstander = naamTegenstander;
+        this.voorDoelpunten = voorDoelpunten;
+        this.tegenDoelpunten = tegenDoelpunten;
+    }
 
     public int bepaalWedstrijdpunten() {
-        return 0;
+        if (voorDoelpunten > tegenDoelpunten) {
+            return 3;
+        } else if (voorDoelpunten == tegenDoelpunten) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
