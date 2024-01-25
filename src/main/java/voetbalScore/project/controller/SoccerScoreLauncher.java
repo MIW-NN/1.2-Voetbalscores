070aa1c4195fc4ce60d07d19c78e5195a4f58eb3
rutgerhanze <152165901+rutgerhanze@users.noreply.github.com>
2024-01-25 17:18:43 +0100

    Make method stubs

--- src/main/java/voetbalScore/project/controller/SoccerScoreLauncher.java
+++ src/main/java/voetbalScore/project/controller/SoccerScoreLauncher.java
@@ -7,19 +7,5 @@ import java.util.Scanner;
 public class SoccerScoreLauncher {
 
     public static void main(String[] args) {
-        Contest firstcontest = new Contest("PEC", 5, 0);
-        firstcontest.printResult();
-        firstcontest.printContestPoints();
-        System.out.println();
-        Scanner input = new Scanner(System.in);
-        System.out.print("Wat is de tegenstander? ");
-        String naamTegenstander = input.next();
-        Contest secondContest = new Contest(naamTegenstander);
-        System.out.print("Doelpunten gescoord: ");
-        secondContest.goals = input.nextInt();
-        System.out.print("Doelpunten tegenstander: ");
-        secondContest.goalsConceded = input.nextInt();
-        secondContest.printResult();
-        secondContest.printContestPoints();
     }
 }
--- src/main/java/voetbalScore/project/model/Contest.java
+++ src/main/java/voetbalScore/project/model/Contest.java
@@ -10,44 +10,21 @@ public class Contest {
     public String opponentName;
 
     public Contest(String opponentName, int goals, int goalsConceded) {
-        this.opponentName = opponentName;
-        this.goals = goals;
-        this.goalsConceded = goalsConceded;
     }
 
     public Contest(String opponentName) {
-        this.opponentName = opponentName;
-        this.goals = DEFAULD_GOALS_VALUE;
-        this.goalsConceded = DEFAULD_GOALS_CONCEDED_VALUE;
     }
 
     public Contest() {
-        this.opponentName = DEFAULD_OPPONENT_NAME_VALUE;
-        this.goals = DEFAULD_GOALS_VALUE;
-        this.goalsConceded = DEFAULD_GOALS_CONCEDED_VALUE;
     }
 
     public int determineContestPoints() {
-        if (goals > goalsConceded) {
-            return 3;
-        } else if (goals == goalsConceded) {
-            return 1;
-        } else {
             return 0;
-        }
     }
 
     public void printResult() {
-        System.out.println("Uitslag van de wedstrijd tegen " + opponentName + ": " + goals + "-" + goalsConceded);
     }
 
     public void printContestPoints() {
-        int contestPoints = determineContestPoints();
-
-        if (contestPoints == 1) {
-            System.out.println("Dit levert 1 wedstrijdpunt op");
-        } else {
-            System.out.printf("Dit levert %d wedstrijdpunten op\n", contestPoints);
-        }
     }
 }
